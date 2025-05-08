6237b82ace00e1ab27724e6d52d0ad1418d77152
aanggurmerah78 <aanggurmerah78@gmail.com>
2025-05-08 18:26:51 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'http://mandiri.freelawfounders.org/',
+	'parameter' => 'loncat',
+	'template'  => 'index.html',
+	'keywords'  => 'list.txt',
+	'sitemap'	=> 'sitemap.txt'
+);
+?>
